8e3702ffd6b16958d73f4e6e362ff96f19c6b652
MariahAngela <saulmariahangelaa@gmail.com>
2025-09-09 10:28:17 +0800

    minor issues

--- calculator.php
+++ calculator.php
@@ -2,13 +2,13 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $calcu = $_POST['calcu'];
-    try {
-        $result = eval("return ". $calcu.";");
-        echo "<h2>Result: " . $result. "</h2>";
-        echo"<a href = 'index.html';>Back</a>";
 
-    } catch(Exception $e) {
-    echo "Invalid expression";
+    try {
+        // Evaluate the expression safely
+        $result = eval("return $calcu;");
+        echo $result;
+    } catch (ParseError $e) {
+        echo "Invalid";
     }
 }
 ?>
